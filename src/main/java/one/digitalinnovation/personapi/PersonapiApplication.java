afc87c810d518950ab27adc0cbb7189c5b318bf3
kyo1506 <vinicius_pinheiro02@hotmail.com>
2021-08-26 16:38:09 -0300

    Initial

--- src/main/java/one/digitalinnovation/personapi/PersonapiApplication.java
+++ src/main/java/one/digitalinnovation/personapi/PersonapiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PersonapiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PersonapiApplication.class, args);
 	}
-
 }
